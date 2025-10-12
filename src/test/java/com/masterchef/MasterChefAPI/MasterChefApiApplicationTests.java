7c2fe363ab0498dcb071d891f82bee9698cb82cc
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2025-10-12 12:21:27 -0500

    project

---
+++ src/test/java/com/masterchef/MasterChefAPI/MasterChefApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterchef.MasterChefAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterChefApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
